83c5e988199696bb3562a844aea364356b4d55ad
Michael Anderson <michanders@msn.com>
2018-03-04 22:16:05 +0000

    automatically backed up by learn

--- components/Autocomplete.js
+++ components/Autocomplete.js
@@ -11,7 +11,7 @@ class Autocomplete extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-			query: '',
+			query: "",
 			results: resultStore.getState().results
     };
 		this.handleChange = this.handleChange.bind(this);
