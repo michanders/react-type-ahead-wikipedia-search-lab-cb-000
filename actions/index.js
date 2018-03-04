8a4418c32688dd3a9febccb9c5b6fb8569707ca9
Michael Anderson <michanders@msn.com>
2018-03-04 21:56:19 +0000

    automatically backed up by learn

--- actions/index.js
+++ actions/index.js
@@ -8,8 +8,23 @@ const search = (query) => {
   const requested = new Date();
 
   return wikipedia.search(query).then((data) => {
-    // TODO
+    if (resultStore.isOutdated(requested)){
+			return;
+		}
+
+		const [query,titles,descriptions,links] = data;
+		const results = titles.map((title, i) => ({
+			title,
+			description: descriptions[i],
+			link: links[i]
+		}));
+
+		resultStore.setState({
+			results,
+			updated: requested
+		});
   });
 };
+
 
 export default { search };
