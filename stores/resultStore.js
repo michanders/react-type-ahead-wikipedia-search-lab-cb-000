14d031c2afa6d70cb3ad61a0af22588b42d43348
Michael Anderson <michanders@msn.com>
2018-03-04 22:04:20 +0000

    automatically backed up by learn

--- stores/resultStore.js
+++ stores/resultStore.js
@@ -5,6 +5,8 @@ import Store from './Store';
 class ResultStore {
 }
 
-const resultStore = new ResultStore();
-
+const resultStore = new ResultStore({
+	results: [],
+	updated: new Date()
+});
 export default resultStore;
